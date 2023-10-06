f3f57cd759767dfc765be88ec0c8b5d1374bed92
suelgp8 <francegp8@gmail.com>
2023-10-06 00:57:10 -0300

    texto do icone

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,9 +4,9 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center text-xs ">
                     <a href="{{ route('dashboard') }}">
-                       <img  class="block h-9 w-auto fill-current text-gray-800" src="icon-dep.jpeg" alt="icone"> 
+                       <img  class="block h-9 w-auto fill-current text-gray-800" src="icon-dep.jpeg" alt="icone"> LineDep
                     </a>
                 </div>
 
